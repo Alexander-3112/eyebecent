370f01dc9070e675fb7bf69c041294156944db1a
Farid Azhari <farid@ganemid.com>
2023-09-12 15:30:42 +0700

    Initial Commit

---
+++ eyebecent/eyebecentApp.swift
@@ -0,0 +1,17 @@
+//
+//  eyebecentApp.swift
+//  eyebecent
+//
+//  Created by Farid Azhari on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct eyebecentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
